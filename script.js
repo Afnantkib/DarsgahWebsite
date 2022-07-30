ae8368810ae524a71d7fdf0535a7da3e6f3d14e2
Mohammad Afnan <afnantkib@gmail.com>
2022-07-30 23:41:47 +0530

    Minore changes in quiz

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ let scoreSpan=document.getElementById("score");
 let buttons=document.querySelectorAll("button");
 buttons.forEach(button => {
     button.addEventListener("click",(e)=>{
-        let btn=e.target;
-        if(btn.getAttribute("key")==="true"){
-            btn.setAttribute("key","");
-            let prevScore=parseInt(scoreSpan.innerHTML);
+        let keyValue=e.target.getAttribute("key");
+        let prevScore=parseInt(scoreSpan.innerHTML);
+        if(keyValue==="true"){
             prevScore++;
+            scoreSpan.innerHTML=prevScore;
+            e.target.setAttribute("key","");
+        }else if(keyValue==="false"){
+            prevScore--;
             scoreSpan.innerHTML=prevScore;
         }
     })
